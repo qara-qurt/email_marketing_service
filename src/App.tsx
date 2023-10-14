6aa232256bd36e8dedbbc32d699bc7eb7dbd28c0
qara-qurt <serikov.2002.12@gmail.com>
2023-10-14 15:26:33 +0600

    init app and configured eslint/prettier

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Home from "@pages/Home/Home.tsx";
+
+function App() {
+	return (
+		<Home></Home>
+	)
+}
+
+export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyComponent;
