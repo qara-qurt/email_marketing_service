2d9a070364e7b146ff2cbce528abe2ea6de515a6
qara-qurt <serikov.2002.12@gmail.com>
2023-11-08 21:22:09 +0600

    added signup signin

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import HomePage from "pages/Home/HomePage.tsx";
 import { Route, Routes } from "react-router-dom";
 import EmailService from "pages/EmailService/EmailService.tsx";
 import SignIn from "pages/SignIn/SignIn.tsx";
+import SignUp from "pages/SignUp/SignUp.tsx";
 
 function App() {
 	return (
 		<Routes>
 			<Route index element={<HomePage />}/>
 			<Route path={"sign-in"} element={<SignIn />} />
+			<Route path={"sign-up"} element={<SignUp />} />
 			<Route path={"emailservice"} element={<EmailService />}/>
 		</Routes>
 	)
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,11 +1,35 @@
 import { GoogleLogin } from '@react-oauth/google';
 import styles from "./SignIn.module.css";
 import Button from "components/Button/Button.tsx";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const SignIn = () => {
+
+  const navigate = useNavigate()
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const responseMessage = (response:any) => {
     console.log(response);
   };
+
+  const handleEmail = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    setEmail(e.target.value)
+  }
+
+  const handlePassword = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    setPassword(e.target.value)
+  }
+
+  const handleSignIn = () => {
+    const data = {
+      email:email,
+      password: password,
+    }
+
+    console.log(data);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -13,14 +37,18 @@ const SignIn = () => {
         <div className={styles.sign_in}>
           <h2>Sign-in</h2>
           <form action="">
-            <input type="text"  placeholder={"email"}/>
-            <input type="text"  placeholder={"password"}/>
-            <Button>Зарегестироваться</Button>
+            <input type="text"  placeholder={"email"} value={email} onChange={handleEmail}/>
+            <input type="password"  placeholder={"password"} value={password} onChange={handlePassword}/>
+            <Button color={"#fff"} bg_color={"#ff5d5d"} onClick={handleSignIn}>Войти</Button>
           </form>
+          <GoogleLogin onSuccess={responseMessage} onError={()=>"error"} />
         </div>
-
       </div>
-      <GoogleLogin onSuccess={responseMessage} onError={()=>"error"} />
+      <AiOutlineArrowLeft
+        onClick={()=>{
+          navigate("/")
+          }}
+        style={{position:'absolute',top:'50px',left:'50px',fontSize:32, color:'#fff',cursor:'pointer'}}/>
     </div>
   );
 };
